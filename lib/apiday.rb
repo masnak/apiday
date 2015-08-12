d39eb2293e423925389dbf98c5751349539c319c
okamuratakeshi <vagrant@enpit>
2015-08-12 01:55:40 +0000

    天気情報取得の追加

--- lib/apiday.rb
+++ lib/apiday.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 #require "enpit/birthday/version"
 require 'wikipedia'
+require 'csv'
 
 module Apiday
  class Main
@@ -60,11 +61,36 @@ module Apiday
        return "== " + month.to_s + "月" + day.to_s + "日 ==\n" + page_day.content[index,index2-index]
     end
     
+    def tokyo_wheather(year,month,day)
+        #カレントディレクトリ取得
+        path =  Dir.pwd
+        
+        #datディレクトリ配下のCSVを読み込み（1967-2014_tokyo_weather.csv）
+        datas = CSV.read(path + "/dat/1967-2014_tokyo_weather.csv")
+        
+        #検索キーの設定（年/月/日）
+        keydate = year.to_s + "/" + month.to_s + "/" + day.to_s
+        
+        #検索結果の初期化
+        result = year.to_s + "年" + month.to_s + "月" + day.to_s + "日の天気情報がありません"
+
+        #ファイルの最後まで繰り返し
+        datas.each do |data|
+           #検索キーと1列目が一致したら、2列目を結果にセット
+           if data[0] == keydate then
+              result = year.to_s + "年" + month.to_s + "月" + day.to_s + "日の東京地方のお天気は「" + data[1] + "」でした！"
+           end
+        end
+
+        #結果の返却
+        return result
+    end
+    
     # メソッド呼び出し部
     #year, month, day = Main.new.input
     puts Main.new.wikipedia_year(year)
     puts Main.new.wikipedia_day(month, day)
-
+    puts Main.new.tokyo_wheather(year,month,day)
 
 end
 end
